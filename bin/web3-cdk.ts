97e795cdf825e7096b0521a5ec53b4cfbfd14461
ha-king <ha-king@users.noreply.github.com>
2025-06-29 20:40:44 +0000

    Remove dev stack from CDK app, keep existing dev infrastructure

--- bin/web3-cdk.ts
+++ bin/web3-cdk.ts
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { Web3ProdStack } from '../lib/web3-prod-stack';
-import { Web3DevStack } from '../lib/web3-dev-stack';
 
 const app = new cdk.App();
 
-// Combined stacks with app and pipeline
+// Production stack only
 new Web3ProdStack(app, 'Web3ProdStack', {});
-new Web3DevStack(app, 'Web3DevStack', {});
