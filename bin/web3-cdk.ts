bbbb1af26fd8994d3f8d8aab4a7bb7127a11abba
ha-king <ha-king@users.noreply.github.com>
2025-06-29 21:04:46 +0000

    Update dev pipeline to use buildspec-dev.yml

--- bin/web3-cdk.ts
+++ bin/web3-cdk.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { Web3ProdStack } from '../lib/web3-prod-stack';
+import { Web3DevStack } from '../lib/web3-dev-stack';
 
 const app = new cdk.App();
 
-// Production stack only
+// Combined stacks
 new Web3ProdStack(app, 'Web3ProdStack', {});
+new Web3DevStack(app, 'Web3DevStack', {});
