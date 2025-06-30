fd0d3ec496e96110124d51db0214b0665c33f5c3
ha-king <003king@gmail.com>
2025-06-30 23:09:02 +0000

    Add specific Base NFT contract from OpenSea transaction

--- app.js
+++ app.js
@@ -58,16 +58,16 @@ const APECHAIN_NFT_CONTRACTS = [
 
 const BASE_NFT_CONTRACTS = [
     {
+        address: '0x50f5474724e0ee42d9a4e711ccfb275809fd6d4a',
+        name: 'Base OpenSea Collection',
+        description: 'NFT Collection from OpenSea on Base',
+        creator: '0x50f5474724e0ee42d9a4e711ccfb275809fd6d4a'
+    },
+    {
         address: '0xd4307e0acd12cf46fd6cf93bc264f5d5d1598792',
         name: 'Base Gods',
         description: 'Base Gods NFT Collection',
         creator: '0xd4307e0acd12cf46fd6cf93bc264f5d5d1598792'
-    },
-    {
-        address: '0x1538c5ddbb073638b7cd1ae41ec2d9f9a4c24a7e',
-        name: 'Coinbase Wallet NFT',
-        description: 'Coinbase Wallet NFT Collection',
-        creator: '0x1538c5ddbb073638b7cd1ae41ec2d9f9a4c24a7e'
     }
 ];
 
