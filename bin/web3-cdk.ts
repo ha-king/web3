86c514f7d0be8a0502bfa097d56588ee75f056dc
Ubuntu <ubuntu@ip-172-31-19-97.us-west-2.compute.internal>
2025-06-30 17:09:48 +0000

    test branch

--- bin/web3-cdk.ts
+++ bin/web3-cdk.ts
@@ -2,9 +2,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Web3ProdStack } from '../lib/web3-prod-stack';
 import { Web3DevStack } from '../lib/web3-dev-stack';
+import { Web3Dev2Stack } from '../lib/web3-dev2-stack';
 
 const app = new cdk.App();
 
 // Combined stacks
 new Web3ProdStack(app, 'Web3ProdStack', {});
 new Web3DevStack(app, 'Web3DevStack', {});
+new Web3Dev2Stack(app, 'Web3Dev2Stack', {});
