0724e5ccb183eb2d4172bca77a1c1fe152cec1d0
Ubuntu <ubuntu@ip-172-31-19-97.us-west-2.compute.internal>
2025-06-30 17:37:34 +0000

    addnig prod

--- bin/web3-cdk.ts
+++ bin/web3-cdk.ts
@@ -4,6 +4,7 @@ import { Web3ProdStack } from '../lib/web3-prod-stack';
 import { Web3DevStack } from '../lib/web3-dev-stack';
 import { Web3Dev2Stack } from '../lib/web3-dev2-stack';
 import { Web3TestStack } from '../lib/web3-test-stack';
+import { Web3Prod2Stack } from '../lib/web3-prod2-stack';
 
 const app = new cdk.App();
 
@@ -12,3 +13,4 @@ new Web3ProdStack(app, 'Web3ProdStack', {});
 new Web3DevStack(app, 'Web3DevStack', {});
 new Web3Dev2Stack(app, 'Web3Dev2Stack', {});
 new Web3TestStack(app, 'Web3TestStack', {});
+new Web3Prod2Stack(app, 'Web3Prod2Stack', {});
